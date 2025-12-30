669ef7f4a4a3ba04e776b26d87f07e851100b7e5
srenV <225722508+voigtsren-source@users.noreply.github.com>
2025-12-30 20:57:26 +0100

    mobile layout done

---
+++ app.js
@@ -0,0 +1,7 @@
+const backButton = document.getElementById('backButton')
+const forwardButton = document.getElementById('forwardButton')
+
+const firstItems = document.querySelectorAll('.first')
+const secondItems = document.querySelectorAll('.second')
+const thirdItems = document.querySelectorAll('.third')
+
