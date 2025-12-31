223a87a206858b9bfadcaa527d9be9111a921ac4
srenV <225722508+voigtsren-source@users.noreply.github.com>
2025-12-31 10:53:16 +0100

    logic implementation

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const backButton = document.getElementById('backButton')
 const forwardButton = document.getElementById('forwardButton')
 
-const firstItems = document.querySelectorAll('.first')
-const secondItems = document.querySelectorAll('.second')
-const thirdItems = document.querySelectorAll('.third')
+const items = document.querySelectorAll('.item')
+const images = document.querySelectorAll('.image')
 
+let currentIndex = 0
+
+backButton.addEventListener('click', () =>{
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    updateSection()
+})
+
+forwardButton.addEventListener('click', () =>{
+    currentIndex = (currentIndex + 1) % images.length
+    updateSection()
+})
+
+function updateSection() {
+    images.forEach((img, index) => {
+        if (index === currentIndex) {
+            img.style.display = 'block'
+        } else {
+            img.style.display = 'none'
+        }
+    })
+
+    items.forEach((itm, index) => {
+        if (index === currentIndex) {
+            itm.style.display = 'block'
+        } else {
+            itm.style.display = 'none'
+        }
+    })
+}
